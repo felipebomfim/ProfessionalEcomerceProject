c11ef29bee58e5e8430cf75d2128856623db7089
Felipe Bomfim <felipe.bomfim@camara.leg.br>
2025-01-23 13:51:50 -0300

    Created first spring-boot project

---
+++ first_spring/src/main/java/com/embarkx/first_spring/HelloController.java
@@ -0,0 +1,15 @@
+package com.embarkx.first_spring;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class HelloController {
+    
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
+    
+}
