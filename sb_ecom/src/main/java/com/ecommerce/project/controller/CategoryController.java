2d2acbb933d0242d89fcd74816ccb4bb3d630464
Felipe Bomfim <felipe.bomfim@camara.leg.br>
2025-01-23 17:21:10 -0300

    Implemented Category creation endpoint

--- sb_ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb_ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.project.model.Category;
@@ -16,4 +18,11 @@ public class CategoryController {
     public List<Category> getAllCategories(){
         return categories;
     }
+
+    @PostMapping("/api/admin/category")
+    public String createCategory(@RequestBody Category category) {
+        categories.add(category);        
+        return "Category added successfully";
+    }
+    
 }
