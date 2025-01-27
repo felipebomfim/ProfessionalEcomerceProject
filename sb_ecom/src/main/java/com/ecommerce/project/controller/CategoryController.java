6aeb4cbf8954b9d8246f513c365bbf11908d954b
Felipe Bomfim <felipe.bomfim@camara.leg.br>
2025-01-27 09:13:48 -0300

    Created the CategoryService interface and its implementation

--- sb_ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb_ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -1,6 +1,5 @@
 package com.ecommerce.project.controller;
 
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,19 +8,25 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ecommerce.project.model.Category;
+import com.ecommerce.project.service.CategoryService;
 
 @RestController
 public class CategoryController {
-    private List<Category> categories = new ArrayList<>();
+    
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService) {
+        this.categoryService = categoryService;
+    }
 
     @GetMapping("/api/public/categories")
     public List<Category> getAllCategories(){
-        return categories;
+        return categoryService.getAllCategories();
     }
 
     @PostMapping("/api/admin/category")
     public String createCategory(@RequestBody Category category) {
-        categories.add(category);        
+        categoryService.createCategory(category);        
         return "Category added successfully";
     }
     
---
+++ sb_ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.ecommerce.project.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.ecommerce.project.model.Category;
+
+@Service
+public interface CategoryService {
+    
+    List<Category> getAllCategories();
+
+    void createCategory(Category category);
+}
