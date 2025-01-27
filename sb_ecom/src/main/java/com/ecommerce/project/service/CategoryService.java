b5152328c495f75ecefe689078074b02311acc41
Felipe Bomfim <felipe.bomfim@camara.leg.br>
2025-01-27 09:34:51 -0300

    Implemented deletion endpoint

--- sb_ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
+++ sb_ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -12,4 +12,6 @@ public interface CategoryService {
     List<Category> getAllCategories();
 
     void createCategory(Category category);
+
+    String deleteCategory(Long categoryId);
 }
--- sb_ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb_ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -11,6 +11,7 @@ import com.ecommerce.project.model.Category;
 public class CategoryServiceImpl implements CategoryService{
 
     private List<Category> categories = new ArrayList<>();
+    private Long nextId = 1L;
 
     @Override
     public List<Category> getAllCategories() {
@@ -19,7 +20,22 @@ public class CategoryServiceImpl implements CategoryService{
 
     @Override
     public void createCategory(Category category) {
+        category.setCategoryId(nextId++);
         categories.add(category); 
+    }
+
+    @Override
+    public String deleteCategory(Long categoryId) {
+        Category category = categories.stream()
+                                    .filter(c->c.getCategoryId().equals(categoryId))
+                                    .findFirst()
+                                    .orElse(null);
+
+        if (category == null)
+            return "Category not found";
+
+        categories.remove(category);
+        return "Category with categoryId: "+categoryId+" deleted successfully!!";
     }
 
 }
