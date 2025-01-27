f429da92856e4d1fed16818d807e601253f6440b
Felipe Bomfim <felipe.bomfim@camara.leg.br>
2025-01-27 14:04:17 -0300

    Applied the request mapping annotation to the CategoryController

--- sb_ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ sb_ecom/src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -17,6 +18,7 @@ import com.ecommerce.project.model.Category;
 import com.ecommerce.project.service.CategoryService;
 
 @RestController
+@RequestMapping("/api")
 public class CategoryController {
     
     private CategoryService categoryService;
@@ -25,19 +27,19 @@ public class CategoryController {
         this.categoryService = categoryService;
     }
 
-    @GetMapping("/api/public/categories")
+    @GetMapping("/public/categories")
     public ResponseEntity<List<Category>> getAllCategories(){
         List<Category> categories = categoryService.getAllCategories();
         return new ResponseEntity<>(categories, HttpStatus.OK);
     }
 
-    @PostMapping("/api/admin/category")
+    @PostMapping("/admin/category")
     public ResponseEntity<String> createCategory(@RequestBody Category category) {
         categoryService.createCategory(category);        
         return new ResponseEntity<>("Category added successfully",HttpStatus.CREATED);
     }
 
-    @DeleteMapping("/api/admin/categories/{categoryId}")
+    @DeleteMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
         try{
             String status = categoryService.deleteCategory(categoryId);
@@ -49,7 +51,7 @@ public class CategoryController {
         }
     }
 
-    @PutMapping("/api/admin/categories/{categoryId}")
+    @PutMapping("/admin/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(
         @RequestBody Category category, 
         @PathVariable Long categoryId
