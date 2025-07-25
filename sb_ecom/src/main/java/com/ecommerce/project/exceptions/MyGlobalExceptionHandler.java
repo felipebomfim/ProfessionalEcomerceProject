fcd8f18c72db7e4989f0be978c06a6aaa4edcc73
Felipe Bomfim <felipebomfim4161@gmail.com>
2025-07-25 13:31:49 -0300

    Criado Controller Advice

---
+++ sb_ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -0,0 +1,43 @@
+package com.ecommerce.project.exceptions;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class MyGlobalExceptionHandler {
+
+    @ResponseBody
+	@ExceptionHandler(Exception.class)
+	public ResponseEntity<String> handleAllExceptions(Exception ex) {
+		return ResponseEntity.internalServerError().body("An error occurred: " + ex.getMessage());
+	}
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public Map<String, String>  handleArgumentNotValidException(MethodArgumentNotValidException e){
+        Map<String, String> response = new HashMap<>();
+        e.getBindingResult().getAllErrors().forEach(err -> {
+            String fieldName = ((FieldError)err).getField();
+            String message = err.getDefaultMessage();
+            response.put(fieldName, message);
+        });
+        return response;
+    }
+
+    @ExceptionHandler(ResourceNotFoundException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public String handleResourceNotFound(ResourceNotFoundException e){
+        String response = e.getMessage();
+        return response;
+    }
+
+}
