6546e47dc879c734d0e7708d0752b7579faf389d
Felipe Bomfim <felipebomfim4161@gmail.com>
2025-07-25 11:00:55 -0300

    Removida anotação @Service desnecessária da interface

--- sb_ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
+++ sb_ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -2,11 +2,8 @@ package com.ecommerce.project.service;
 
 import java.util.List;
 
-import org.springframework.stereotype.Service;
-
 import com.ecommerce.project.model.Category;
 
-@Service
 public interface CategoryService {
     
     List<Category> getAllCategories();
